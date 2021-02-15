e2862c19c91dfd4dc6d38625ac7d5f947faf2669
Abdullah Mazen Al-Haddad <57428346+Leddark@users.noreply.github.com>
2021-02-15 17:07:45 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'z04+kg(e0#mnnt202d!35*^662=(9d$mhv^+nv(qbuzz_zolan'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'tad',
-#        'USER': 'postgres',
-#        'PASSWORD' : 'adrelaft',
-#        'Host': 'localhost',
-#        'PORT': '5432',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tad',
+        'USER': 'postgres',
+        'PASSWORD' : 'adrelaft',
+        'Host': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 DATABASES = {
 	'default': dj_database_url.config()
@@ -160,4 +160,4 @@ django_heroku.settings(locals())
 # Heroku: Update database configuration from $DATABASE_URL.
 
 #db_from_env = dj_database_url.config(conn_max_age=500)
-
+db_from_env = dj_database_url.config(conn_max_age=500)
